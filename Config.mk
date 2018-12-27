70872b45a117501017bcd80a909242a737cb5486
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2018-12-27 15:26:03 -0200

    Add flags and info in Config.mk

--- Config.mk
+++ Config.mk
@@ -1,49 +1,26 @@
 
-############################################################
-##     UNCOMMENT ANY TARGET TO OVERWRITE THE DEFAULT!     ##
-############################################################
+################################################################################
+##                                MULTIVECTOR                                 ##
+################################################################################
 
 # Project info
 # ===============
-# PROJECT         := # Project name (def: Default)
-# VERSION         := # Version (def: 1.0.0)
-# STD_NAMESPACE   := # Project namespace for C/C++
-# GIT_REMOTE_PATH := # Remote path for git repository
-# MAINTAINER_NAME := # Your name
-# MAINTAINER_MAIL := # your_name@mail.com
-# COPYRIGHT       := # Copyright Holder
-# SYNOPSIS        := # One-line description of the program
-# DESCRIPTION     := # Longer description of the program
-
-# Libraries
-# ===========
-# ARLIB           := # Static libraries (file/dir in srcdir)
-# SHRLIB          := # Shared libraries (file/dir in srcdir)
-
-# Dependencies
-# ==============
-# GIT_DEPENDENCY  := # List of git dependencies in the form: 
-#                    # DEP_NAME => dep_path build_cmd        
-# WEB_DEPENDENCY  := # Same as above, but for URL downloads  
-#                    # with 'curl -o' (default) or 'wget -O' 
-
-# Paths
-# =======
-# ASLIBS          := # Assembly paths
-# CLIBS           := # C paths
-# CXXLIBS         := # C++ paths
-# LDLIBS          := # Linker paths
+PROJECT         := Multivector
+VERSION         := 0.1.0
+STD_NAMESPACE   := # multivector
+GIT_REMOTE_PATH := # Remote path for git repository
+MAINTAINER_NAME := Renato Cordeiro Ferreira
+MAINTAINER_MAIL := renato.cferreira@hotmail.com
+COPYRIGHT       := Renato Cordeiro Ferreira
+SYNOPSIS        := Linearized multi-dimensional vectors
+DESCRIPTION     := Linearized multi-dimensional vectors with a simple syntax
 
 # Flags
 # =======
-# CPPFLAGS        := # Precompiler Flags
-# ASFLAGS         := # Assembly Flags
-# CFLAGS          := # C Flags
-# CXXFLAGS        := # C++ Flags
-# LDFLAGS         := # Linker flags
+CPPFLAGS        := # Precompiler Flags
+CXXFLAGS        := -std=c++17 -Wall -Wextra -Wpedantic -Wshadow -O3
+LDFLAGS         := # Linker flags
 
 # Makeball list
 # ===============
-# 'include conf/makeball.mk' for pre-configured options
-# to use the library 'makeball'
-
+include conf/googletest.mk
